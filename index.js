383b87d493ed464edb171f0b8df140b47ef44aad
Jeffmwai <Jefylionel@gmail.com>
2023-10-10 01:30:39 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,17 +1,11 @@
-// Code your solutions in this file
-// Function to generate thank you messages for names
 function writeCards(names, occasion) {
     return names.map(name => `Thank you, ${name}, for the wonderful ${occasion} gift!`);
   }
-  
-  // Function to count down from a given number to zero and log each number
   function countDown(number) {
     for (let i = number; i >= 0; i--) {
       console.log(i);
     }
   }
-  
-  // Export the functions for testing
   module.exports = {
     writeCards,
     countDown,
